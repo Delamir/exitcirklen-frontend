28e52f859714ed7905b3ca928b10015946c51bab
Kringelum2 <p.kringelum@gmail.com>
2022-11-15 14:12:59 +0100

    Group table added

---
+++ src/components/GroupTable.js
@@ -0,0 +1,19 @@
+
+import Table from "./Table";
+
+function GroupTable() {
+
+    const headers = ["By", "Adresse", "Gruppe Navn", "Hold Størrelse", "Ledige Pladser", "Pris", "Datoer for start", "Handlinger"];
+    return <Table headers={headers}>
+        <td>ugg</td>
+        <td>ugg</td>
+        <td>ugg</td>
+        <td>ugg</td>
+        <td>ugg</td>
+        <td>ugg</td>
+        <td>ugg</td>
+        <td>ugg</td>
+
+    </Table>
+}
+export default GroupTable;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,16 @@
+import {useEffect, useState} from "react";
 
 
 function Table(props) {
-
-    const { headers } = props;
+    const headers = props.headers
+    // const [headers, setHeaders] = useState([]);
+    // useEffect(()=> setHeaders(props.headers),[])
 
     return (
         <Table striped bordered hover size="large">
             <thead className="bg-primary">
             <tr>
-                { headers.map((header) => <th>{header}</th>) }
+                { headers?.map((header, index) => <th key={index}>{header}</th>) }
             </tr>
             </thead>
             <tbody>
