1824c35d29dbc895fd490ed79d65d98eee3f6cec
Delamir <td_christian@hotmail.com>
2022-11-15 14:17:22 +0100

    Client table update

--- src/components/ClientTable.js
+++ src/components/ClientTable.js
@@ -1,13 +1,12 @@
-
 import Table from "./Table";
 
 function ClientTable() {
 
-    const headers = ["Navn", "Alder", "E-mail", "Telefonnummer", "Status", "Længde status", ""]
+    const headers = ["Navn", "Alder", "E-mail", "Telefonnummer", "Status", "Længde status", "Lokation",
+        "Bemærkning", "Handlinger"]
 
     return (
-
-        <Table />
+            <Table props={headers} />
     );
 }
 
