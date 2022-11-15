e1df73cf8d526d4d9e8965e8e01330f7f3b8be8b
Sverri Reinert <sverrireinert@gmail.com>
2022-11-15 12:36:33 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import ApplicantForm from "./components/ApplicantForm";
+
 function App() {
-    return <p>Hejsa</p>;
+    return (
+        <Routes>
+            <Route path="/tilmeld" element={<ApplicantForm />} />
+        </Routes>
+    );
 }
 
 export default App;
