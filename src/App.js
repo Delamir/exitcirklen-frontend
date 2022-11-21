ef332aabd5aeb2feb19ecf6740aa1297e7b3fe91
Sverri Reinert <sverrireinert@gmail.com>
2022-11-21 12:31:01 +0100

    d

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ApplicantForm from "./components/ApplicantForm";
 import Navbar from "./components/Navbar";
 import ClientTable from "./components/ClientTable";
 import GroupTable from "./components/GroupTable";
+import Email from "./components/Email";
 
 function App() {
     return (
         <>
-            <Navbar>
-
-            </Navbar>
+            <Navbar></Navbar>
 
             <Routes>
-                <Route path="/tilmeld" element={<ApplicantForm/>}/>
-                <Route path="/klientoversigt" element={<ClientTable/>} />
-                <Route path="/gruppeoversigt" element={<GroupTable/>} />
+                <Route path="/tilmeld" element={<ApplicantForm />} />
+                <Route path="/klientoversigt" element={<ClientTable />} />
+                <Route path="/gruppeoversigt" element={<GroupTable />} />
+                <Route path="/testtest" element={<Email />} />
             </Routes>
-
         </>
     );
 }
