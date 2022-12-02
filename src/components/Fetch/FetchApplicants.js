3632177b3f4577730af6fb8fc62f2f99a8be4af3
Sverri Reinert <sverrireinert@gmail.com>
2022-12-02 13:58:39 +0100

    fdsfs

--- src/components/Fetch/FetchApplicants.js
+++ src/components/Fetch/FetchApplicants.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import authHeader from "../../services/auth-header";
 
 export async function FetchApplicants() {
-    
-    return await axios.get("http://localhost:8081/applicants", authHeader());
+    return await axios.get("http://localhost:8081/applicants", {
+        headers: authHeader(),
+    });
 }
