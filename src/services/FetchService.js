b364819719c81689b3876b26785a91e2d2131d23
Kringelum2 <p.kringelum@gmail.com>
2022-12-05 14:01:49 +0100

    Fetch i en fetchService minus headers

--- src/services/FetchService.js
+++ src/services/FetchService.js
@@ -4,39 +4,27 @@ import authHeader from "./auth-header";
 class FetchService {
 
     async fetchApplicants() {
-        return await axios.get("http://localhost:8081/applicants", {
-            headers: authHeader(),
-        });
+        return await axios.get("http://localhost:8081/applicants");
     }
 
     async fetchApplicantGroups() {
-        return await axios.get("http://localhost:8081/groups", {
-            headers: authHeader(),
-        });
+        return await axios.get("http://localhost:8081/groups");
     }
 
     async fetchApplicantStatus() {
-        return await axios.get("http://localhost:8081/applicants/status", {
-          headers: authHeader(),
-        });
+        return await axios.get("http://localhost:8081/applicants/status");
     }
 
     async fetchEmployees() {
-        return await axios.get("http://localhost:8081/employees", {
-            headers: authHeader(),
-        });
+        return await axios.get("http://localhost:8081/employees");
     }
 
     async fetchWaitingList() {
-        return await axios.get("http://localhost:8081/applicants/waiting-list", {
-            headers: authHeader(),
-        });
+        return await axios.get("http://localhost:8081/applicants/waiting-list");
     }
 
     async fetchEmployeeResponsibility() {
-        return await axios.get("http://localhost:8081/employee/responsibility", {
-            headers: authHeader(),
-        });
+        return await axios.get("http://localhost:8081/employee/responsibility");
     }
 
 }
